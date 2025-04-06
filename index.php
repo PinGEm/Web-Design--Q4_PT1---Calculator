b5c525a140e0012cf9f539a35043fdc7a9e6d98c
Marcus Ureta <silvergreat0@gmail.com>
2025-04-06 22:42:30 +0800

    final calculator

--- index.php
+++ index.php
@@ -7,10 +7,111 @@
     <title>Calculator Application</title>
 </head>
 <body>
-    
+    <div class="base" style="padding-top:0.001%;">
+        <div class="output">
+            <?php
+            session_start();
+
+            if(!isset($_SESSION["output"]))
+            {
+                $_SESSION["output"] = "";
+            }
+
+
+            $value;
+            $output = $_SESSION["output"];
+            $display = "0";
+            $number_array = array('1','2','3','4','5','6','7','8','9');
+            $calc_array = array('1','2','3','4','5','6','7','8','9','-','+','*','/','(',')');
+            if(isset($_GET['number']))
+            {
+                if($output == "0")
+                {
+                    $output = $_GET['number'];
+                }
+                else{
+                    $output .= $_GET['number'];
+                }
+            }
+
+            if(isset($_GET['clear_all']))
+            {
+                $output = 0;
+            }
+
+            if(isset($_GET['operator']))
+            {
+                $output .= $_GET['operator'];
+            }
+            if(isset($_GET['clear_latest']))
+            {
+                $lc_output = substr($output,offset: -1,length: 1);
+                if(in_array($lc_output, $calc_array) == false)
+                {
+                    $output = 0;
+                }
+
+                if (substr($output, 0, -1) == ''){
+                    $output = 0;
+                }
+                else{$output = substr($output, 0, -1);}
+            }
+
+
+            if(isset($_GET['calculate']))
+            {
+                // Calculate the output. Account for any errors as well
+                $lc_output = substr($output,offset: -1,length: 1);
+                if(in_array($lc_output, $number_array) == true)
+                {
+                    $value = eval('return ' . $output . ';');
+                }
+                else
+                {
+                    $value = "ERROR!";
+                    //$value = 0;
+                }
+
+                $lc_output = "";
+
+                $output = $value;
+            }
+
+            $_SESSION["output"] = $output;
+
+            if(!empty($output))
+            {
+                $display = $output; // let's constantly set the output to the display.
+            }
+
+            echo"<p style='margin-right:5%; overflow:hidden;'>$display</p>";
+            ?>
+        </div>
+
+        <div class="input_buttons">
+            <form action="index.php" method="get">
+                <input type="submit" value="C" class="button" name="clear_all" id="clear_all">
+                <input type="submit" value="CE"class="button special" name="clear_latest">
+                <input type="submit" value=")" class="button special" name="number">
+                <input type="submit" value="(" class="button special" name="number">
+                <input type="submit" value="-" class="button" name="operator" id="minus">
+                <input type="submit" value="9" class="button" name="number">
+                <input type="submit" value="8" class="button" name="number">
+                <input type="submit" value="7" class="button" name="number">
+                <input type="submit" value="/" class="button" name="operator" id="divide">
+                <input type="submit" value="6" class="button" name="number">
+                <input type="submit" value="5" class="button" name="number">
+                <input type="submit" value="4" class="button" name="number">
+                <input type="submit" value="*" class="button" name="operator" id="multiply">
+                <input type="submit" value="3" class="button" name="number">
+                <input type="submit" value="2" class="button" name="number">
+                <input type="submit" value="1" class="button" name="number">
+                <input type="submit" value="+" class="button" name="operator" id="addition">
+                <input type="submit" value="=" class="button special" name="calculate">
+                <input type="submit" value="0" class="button" name="number">
+                <input type="submit" value="." class="button" name="number">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
-
-<?php
-// add cool shit here
-?>
